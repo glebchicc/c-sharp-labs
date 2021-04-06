ad11b9441c19cddb2b022a59a704cbe30ab04822
glebchicc <glebbytkevich@tut.by>
2021-04-06 17:06:41 +0300

    fix lab2 files

--- lab2/lab2_1/Program.cs
+++ lab2/lab2_1/Program.cs
@@ -6,9 +6,21 @@ namespace lab2_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите строку");
-            string d = Console.ReadLine();
-            string[] words = d.Split(' '); //расщепляет на слова
+            Console.WriteLine("Введите строку. Допустимы русские и английские символы, а также пробел.");
+            string s = Console.ReadLine();
+            while (s.Contains("  "))
+            {
+                s = s.Replace("  ", " ");
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!(s[i] > 64 && s[i] < 91) && !(s[i] > 96 && s[i] < 123) && !(s[i] > 'А' && s[i] < 'Я') && !(s[i] > 'а' && s[i] < 'я') && s[i] != 'Ё' && s[i] != 'ё' && s[i] != 32)
+                {
+                    Console.WriteLine("Некорректный ввод");
+                    return;
+                }
+            }
+            string[] words = s.Split(' '); //расщепляет на слова
 
             for (int i = words.Length - 1; i >= 0; i--)
             {
--- lab2/lab2_2/Program.cs
+++ lab2/lab2_2/Program.cs
@@ -7,12 +7,12 @@ namespace lab2_2
         static void Main(string[] args)
         {
             DateTime now = DateTime.Now;
-            string date = now.ToString("T");
-            string time = now.ToString("d");
+            string date = now.ToString("d");
+            string time = now.ToString("T");
             int[] numbers = new int[10];
 
-            Console.WriteLine("Текущее время: " + date);
-            Console.WriteLine("Текущая дата: " + time);
+            Console.WriteLine("Текущее время: " + time);
+            Console.WriteLine("Текущая дата: " + date);
 
             for (int i = 0; i < date.Length; i++)
             {
