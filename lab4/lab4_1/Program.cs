7ea24cd8bf75320efa1ae503ab1ea81de87c897a
glebchicc <glebbytkevich@tut.by>
2021-04-12 01:27:02 +0300

    add lab4 files

---
+++ lab4/lab4_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Нажмите клавишу.");
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey();
+                Console.WriteLine("\b" + key.Key + " клавиша была нажата.");
+            }
+            while (key.Key != ConsoleKey.Escape); // по нажатию на Escape завершаем цикл
+        }
+    }
+}
---
+++ lab4/lab4_2/lab4_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace lab4_2
+{
+    static class Program
+    {
+        [DllImport("Dll.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int Add(int a, int b);
+
+        [DllImport("Dll.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int Subtract(int a, int b);
+
+        [DllImport("Dll.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int Multiply(int a, int b);
+
+        [DllImport("Dll.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int Division(int a, int b);
+
+        static int CheckInt()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.Write("Некоректный ввод, повторите попытку: ");
+            }
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Первое число: ");
+                int num1 = CheckInt();
+                Console.Write("Второе число: ");
+                int num2 = CheckInt();
+
+                Console.WriteLine($"{num1} + {num2} = {Add(num1, num2)}");
+                Console.WriteLine($"{num1} - {num2} = {Subtract(num1, num2)}");
+                Console.WriteLine($"{num1} * {num2} = {Multiply(num1, num2)}");
+                Console.WriteLine($"{num1} / {num2} = {Division(num1, num2)}");
+            }
+        }
+    }
+}
