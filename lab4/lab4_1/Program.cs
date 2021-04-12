b7c6b9e4486f958ed337385e882c8c40fe0e1118
glebchicc <glebbytkevich@tut.by>
2021-04-12 18:44:33 +0300

    fix lab4 code

--- lab4/lab4_1/Program.cs
+++ lab4/lab4_1/Program.cs
@@ -1,19 +1,66 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
 
-namespace lab4_1
+namespace ConsoleApplication1
 {
     class Program
     {
+        [DllImport("winmm.dll")]
+        public static extern bool PlaySound(string sound, IntPtr hmod, uint fdwSound);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Нажмите клавишу.");
-            ConsoleKeyInfo key;
-            do
+            bool cont = true;
+            while (cont)
             {
-                key = Console.ReadKey();
-                Console.WriteLine("\b" + key.Key + " клавиша была нажата.");
+                Console.Clear();
+                Console.WriteLine("Выберите аудиофайл для проигрывания: \n0. Стандартный аудиофайл. \n1. Свой аудиофайл.");
+                string pick = Console.ReadLine();
+                if (pick == "1")
+                {
+                    Console.Write("Введите пусть до .wav файла: ");
+                    string filename = Console.ReadLine();
+
+                    FileInfo fileInf = new FileInfo(filename);
+
+                    while (!fileInf.Exists)
+                    {
+                        Console.Write("Не удалось загрузить файл, повторите попытку: ");
+                        fileInf = new FileInfo(Console.ReadLine());
+                    }
+
+                    filename = fileInf.FullName;
+
+                    Console.WriteLine($"\nСейчас играет: {fileInf.Name}\n");
+
+                    PlaySound(filename, IntPtr.Zero, 0x0001);
+
+                    Console.WriteLine($"Нажмите S, чтобы пропустить аудиофайл: ");
+
+                    while (Console.ReadKey().Key != ConsoleKey.S)
+                    {
+                        Console.ReadKey();
+                    }
+
+                    PlaySound(" ", IntPtr.Zero, 0x0001);
+
+                }
+                else if (pick == "0")
+                {
+
+                    Console.WriteLine($"\nThe Weekend - Blinding Lights\n");
+
+                    PlaySound("default.wav", IntPtr.Zero, 0x0001);
+
+                    Console.WriteLine($"Нажмите S, чтобы пропустить аудиофайл: ");
+                    while (Console.ReadKey().Key != ConsoleKey.S)
+                    {
+                        Console.ReadKey();
+                    }
+                    PlaySound(" ", IntPtr.Zero, 0x0001);
+                }
             }
-            while (key.Key != ConsoleKey.Escape); // по нажатию на Escape завершаем цикл
         }
     }
 }
